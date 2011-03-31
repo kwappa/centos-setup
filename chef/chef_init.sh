1c00af09099ce19308f9926c6baa2aff369b3e1d
kwappa <kwappa.856@gmail.com>
2011-03-31 20:13:32 +0900

    add uncommited changes

--- chef/chef_init.sh
+++ chef/chef_init.sh
@@ -1,4 +1,6 @@
-mkdir -m 0777 -p /etc/chef
-mkdir -m 0777 -p /var/chef-solo/cookbooks
-mkdir -m 0777 -p /var/chef-solo/site-cookbooks
+cd ~
+sudo mkdir -m 0777 -p /etc/chef
+sudo mkdir -m 0777 -p /var/chef-solo/cookbooks
+sudo mkdir -m 0777 -p /var/chef-solo/site-cookbooks
 curl https://github.com/kwappa/centos-setup/raw/master/chef/solo.rb > /etc/chef/solo.rb
+git clone git://github.com/opscode/chef-repo.git
