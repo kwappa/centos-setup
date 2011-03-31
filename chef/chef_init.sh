9c56a441f7d01d4ce0fc3b80c9ebe5cc3a358061
kwappa <kwappa.856@gmail.com>
2011-03-31 21:30:45 +0900

    modify mkdir options

--- chef/chef_init.sh
+++ chef/chef_init.sh
@@ -1,6 +1,7 @@
 cd ~
 sudo mkdir -m 0777 -p /etc/chef
-sudo mkdir -m 0777 -p /var/chef-solo/cookbooks
-sudo mkdir -m 0777 -p /var/chef-solo/site-cookbooks
+sudo mkdir -p /var/chef-solo/cookbooks
+sudo mkdir -p /var/chef-solo/site-cookbooks
+sudo chmod -R 0777 /var/chef-solo
 curl https://github.com/kwappa/centos-setup/raw/master/chef/solo.rb > /etc/chef/solo.rb
 git clone git://github.com/opscode/chef-repo.git
